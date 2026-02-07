bdf69b71c0c21cc117c6f0e9f26a4d5edbda565c
Joshua Walker <37340407+joshuanwalker@users.noreply.github.com>
2026-02-07 01:55:09 -0600

    fixed capital cause I'm pedantic

--- make.bat
+++ make.bat
@@ -5,4 +5,4 @@ if not exist out (
 )
 echo Compiling raiders
 if exist out\raiders.bin del out\raiders.bin
-bin\dasm.exe src\Raiders.asm -Isrc -sout\raiders.sym -T1 -Lout\raiders.lst -f3 -oout\raiders.bin
+bin\dasm.exe src\raiders.asm -Isrc -sout\raiders.sym -T1 -Lout\raiders.lst -f3 -oout\raiders.bin
