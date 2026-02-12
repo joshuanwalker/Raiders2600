6449f989435e0dfd4a3c230c16e2c0df12b48767
Joshua Walker <37340407+joshuanwalker@users.noreply.github.com>
2026-02-12 13:15:58 -0600

    Little code reorg

---
+++ src/colors.h
@@ -0,0 +1,20 @@
+;-----------------------------------------------------------
+;	color constants
+;-----------------------------------------------------------
+
+BLACK					= $00
+WHITE					= $0E
+YELLOW					= $10
+LT_RED					= $20
+RED						= $30
+ORANGE					= $40
+DK_PINK					= $50
+PURPLE					= $60
+DK_BLUE					= $70
+BLUE					= $80
+LT_BLUE					= $90
+GREEN_BLUE				= $A0
+GREEN					= $C0
+DK_GREEN				= $D0
+LT_BROWN				= $E0
+BROWN					= $F0
